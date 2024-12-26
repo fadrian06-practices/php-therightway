16de74f1002258df1002e31ec2cda39235b797b3
fadrian06 <franyeradriansanchez@gmail.com>
2024-12-25 22:51:12 -0400

    Video 4

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<body>
+// Variable Variables
 
-  <h1><?= 'Hello World' ?></h1>
-  <p>This is a paragraph.</p>
-  <?php
-  // Comment 1
-  # Comment 2
-  /*
-        Multi line
-       */
-  $x = 10;
-  $y = 5;
+$foo = 'bar';
+$$foo = 'baz';
 
-  echo "<p>$x,$y</p>";
-  ?>
-
-</body>
-
-</html>
+echo "$foo, {$$foo}";
