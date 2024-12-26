61ee50ed99ec2ae54f49e9b877101a0fd39ef004
fadrian06 <franyeradriansanchez@gmail.com>
2024-12-25 22:42:39 -0400

    Video 3

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-Hello World
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <h1><?= 'Hello World' ?></h1>
+  <p>This is a paragraph.</p>
+  <?php
+  // Comment 1
+  # Comment 2
+  /*
+        Multi line
+       */
+  $x = 10;
+  $y = 5;
+
+  echo "<p>$x,$y</p>";
+  ?>
+
+</body>
+
+</html>
