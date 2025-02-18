6e9470bb68a2bd2aa4a697e02d95cc887f38bcef
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-18 19:27:13 -0400

    Video 16

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
-<?php
+<html>
 
-/* Operator Precedence & Associativity */
-$x = true;
-$y = false;
+<body>
+  <?php $score = 55 ?>
 
-$z = ($x and $y);
+  <?php if ($score >= 90): ?>
+    <strong style="color: green">A</strong>
+  <?php elseif ($score >= 80): ?>
+    <strong>B</strong>
+  <?php elseif ($score >= 70): ?>
+    <strong>C</strong>
+  <?php elseif ($score >= 60): ?>
+    <strong>D</strong>
+  <?php else: ?>
+    <strong style="color: red">F</strong>
+  <?php endif ?>
+</body>
 
-var_dump($z);
+</html>
