19f529a86774823ddac21f64f240b9951f7bf622
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-18 19:20:30 -0400

    Video 13

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
 
-// Expressions
+/* OPERATORS */
 
-$x = 5;
+// Arithmetic Operators (+ - * / % **)
 
-$y = $x;
+// Assignment Operators (= += -= *= /= %= **=)
 
-if ($x < 5) {
-  echo 'Hello';
-}
+// String Operators (. .=)
+
+// Comparison Operators (== === != <> !== < > <= >= <=> ?? ?:)
+
+$y = $x ?? 'hello';
+
+var_dump($y);
+
+// Error Control Operators (@)
+
+// Increment/Decrement Operators (++, --)
+
+// Logical Operators (&& || ! and or xor)
+
+// Bitwise Operators (& | ^ ~ << >>)
+
+// Array Operators (+ == === != <> !==)
+
+// Execution Operators (``)
+
+// Type Operators (instanceof)
+
+// Nullsafe Operator - PHP8 (?)
