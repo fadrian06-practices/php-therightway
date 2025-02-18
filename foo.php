ed28b6ad09cbbd21d07ac71c0019501b63bbb053
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-18 19:38:32 -0400

    Video 20

---
+++ foo.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+require 'index.php';
+
+echo sum('5', 10);
--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
 
-$paymentStatus = false;
+/* return | declare | goto */
 
-switch ($paymentStatus) {
-  case 1:
-    echo 'Paid';
-     break;
-  case 2:
-  case 3:
-    echo 'Payment Declined';
-    break;
-  case 0:
-    echo 'Pending Payment';
-    break;
-  default:
-    echo 'Unknown Payment Status';
+declare(strict_types=1);
+
+function sum(int $x, int $y): int {
+  return $x + $y;
 }
-
-echo '<br />';
-
-$paymentStatusDisplay = match ($paymentStatus) {
-  1 > 2 => 'Paid',
-  2, 3 => 'Payment Declined',
-  0 => 'Pending Payment',
-  default => 'Unknown Payment Status'
-};
-
-echo $paymentStatusDisplay;
