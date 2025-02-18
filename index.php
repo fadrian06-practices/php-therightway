cc12fafedaba99411c3516f0a7124df9ff758856
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-18 19:11:05 -0400

    Video 10

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
 <?php
 
-/* STRINGS */
+// NULL
 
-$x = 1;
-$y = 2;
+$x = null;
 
-// Heredoc
-$text = <<<TEXT
- Hello World
-TEXT;
-
-var_dump($text);
-echo nl2br($text);
-
-// Nowdoc
-$text = <<<'TEXT'
-Line 1
-Line 2
-Line 3 ' "
-TEXT;
-
-echo '<br />';
-echo nl2br($text);
+var_dump((array) $x);
