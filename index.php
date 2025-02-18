29990627382a6e476b036dba41e9345923c61359
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-18 18:53:30 -0400

    Video 6

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
 
-/* Data Types & Type Casting */
+/* BOOLEANS */
 
-# 4 Scalar Types
-  # bool - true / false
-  # int - 1, 2, 3, 0, -5 (no decimal)
-  # float - 1.5, 0.1, 0.005, -15.8
-  # string - "Gio", "Hello World"
+$isComplete = 'false';
 
-# 4 Compount Types
-  # array
-  # object
-  # callable
-  # iterable
+// integers 0 -0 = false
+// floats 0.0 -0.0 = false
+// '' = false
+// '0' = false
+// [] = false
+// null = false
 
-# 2 Special Types
-  # resource
-  # null
-
-$x = (int) '5';
-
-var_dump($x);
+if ($isComplete) {
+  // do something
+  echo 'success';
+} else {
+  // do something else
+  echo 'fail';
+}
