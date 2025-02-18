48780681c517f5bfc709915ca0db3fc506290c6d
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-18 18:47:48 -0400

    Video 5

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 
-// Variable Variables
+/* Data Types & Type Casting */
 
-$foo = 'bar';
-$$foo = 'baz';
+# 4 Scalar Types
+  # bool - true / false
+  # int - 1, 2, 3, 0, -5 (no decimal)
+  # float - 1.5, 0.1, 0.005, -15.8
+  # string - "Gio", "Hello World"
 
-echo "$foo, {$$foo}";
+# 4 Compount Types
+  # array
+  # object
+  # callable
+  # iterable
+
+# 2 Special Types
+  # resource
+  # null
+
+$x = (int) '5';
+
+var_dump($x);
